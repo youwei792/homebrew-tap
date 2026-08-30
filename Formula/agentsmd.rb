bc674cb0638dab80898aa4b91bdcc92f96a48a57
youwei792 <229644306+youwei792@users.noreply.github.com>
2026-08-30 17:51:28 +0800

    fix: homebrew strips the top-level tarball directory

--- Formula/agentsmd.rb
+++ Formula/agentsmd.rb
@@ -15,7 +15,7 @@ class Agentsmd < Formula
       sha256 "8f8c618ca44c7693f4a6f6263414acf7af1f55844b1d7d430a8edaf28769cde1"
 
       def install
-        bin.install "agentsmd_darwin_arm64/agentsmd"
+        bin.install "agentsmd"
       end
     end
     on_intel do
@@ -23,7 +23,7 @@ class Agentsmd < Formula
       sha256 "34b916835fa3ad4fc4e114cf6091ad0f3f5c3e4ccf98066c1d5eb8b3cec42a44"
 
       def install
-        bin.install "agentsmd_darwin_amd64/agentsmd"
+        bin.install "agentsmd"
       end
     end
   end
@@ -34,7 +34,7 @@ class Agentsmd < Formula
       sha256 "bd87103b9d491037542e861a68e3ba9cfdd3574831fd14cda47d9ce06f91595e"
 
       def install
-        bin.install "agentsmd_linux_arm64/agentsmd"
+        bin.install "agentsmd"
       end
     end
     on_intel do
@@ -42,7 +42,7 @@ class Agentsmd < Formula
       sha256 "92318d6118b43ed4dcdf2c4d923b65413963473dba320aac8287f0da3cd5cc74"
 
       def install
-        bin.install "agentsmd_linux_amd64/agentsmd"
+        bin.install "agentsmd"
       end
     end
   end
